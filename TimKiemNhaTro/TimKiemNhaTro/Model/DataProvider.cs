b6e2aa0f861fd45ac220b33d2eb3b48ec135b0bb
feelings93 <19521298@gm.uit.edu.vn>
2021-10-23 23:32:18 +0700

    Init db

---
+++ TimKiemNhaTro/TimKiemNhaTro/Model/DataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimKiemNhaTro.Model
+{
+    public class DataProvider
+    {
+        public NhaTroEntities DB;
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+        public DataProvider()
+        {
+            DB = new NhaTroEntities();
+        }
+     
+    }
+}
